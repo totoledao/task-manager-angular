8fe53fc8af7c5cbdf5fe29f68457f46418f8d96f
Guilherme Toledo <totoledao@hotmail.com>
2026-01-03 16:45:48 +0100

    Base layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from '../app/components/header/header';
+import { MainButton } from './components/main-button/main-button';
+import { Tasks } from './components/tasks/tasks';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Tasks, MainButton],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('task-manager');
---
+++ src/app/components/main-button/main-button.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-button',
+  imports: [],
+  templateUrl: './main-button.html',
+})
+export class MainButton {
+  text = 'New task';
+}
