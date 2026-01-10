07280708ac3b0c1bc49041fd0d0d77f1dde3d4b3
Guilherme Toledo <totoledao@hotmail.com>
2026-01-10 15:50:27 +0100

    Refactor Task interface

--- src/app/models/task.ts
+++ src/app/models/task.ts
@@ -1,9 +1,16 @@
 export type TaskStatus = 'todo' | 'doing' | 'completed';
 
+export interface Comment {
+  id: string;
+  description: string;
+}
+
 export interface Task {
   id: string;
   title: string;
   description: string;
+  comments: Comment[];
+  status: TaskStatus;
 }
 
-export type TaskList = Record<TaskStatus, Task[]>;
+export type TaskList = Task[];
--- src/app/services/tasks.ts
+++ src/app/services/tasks.ts
@@ -5,15 +5,16 @@ import { Task, TaskList, TaskStatus } from '../models/task';
   providedIn: 'root',
 })
 export class Tasks {
-  tasks = signal<TaskList>({
-    todo: [],
-    doing: [],
-    completed: [],
-  });
+  tasks = signal<TaskList>([]);
 
   // Get all tasks
   getTasks(): TaskList {
     return this.tasks();
+  }
+
+  // Get tasks by status
+  getTasksByStatus(status: TaskStatus): TaskList {
+    return this.tasks().filter((t) => t.status === status);
   }
 
   // Update all tasks
@@ -22,28 +23,27 @@ export class Tasks {
   }
 
   // Create task
-  addTask(task: Omit<Task, 'id'>) {
-    this.tasks.update((taskList) => ({
+  addTask(task: Omit<Task, 'id' | 'status' | 'comments'>) {
+    this.tasks.update((taskList) => [
       ...taskList,
-      todo: [...taskList.todo, { ...task, id: crypto.randomUUID() }],
-    }));
+      { ...task, status: 'todo', id: crypto.randomUUID(), comments: [] },
+    ]);
   }
 
   // Update status
-  moveTask(task: Task, fromStatus: TaskStatus, toStatus: TaskStatus) {
-    this.tasks.update((taskList) => ({
-      ...taskList,
-      [fromStatus]: taskList[fromStatus].filter((t) => t !== task),
-      [toStatus]: [...taskList[toStatus], task],
-    }));
+  moveTask(task: Task, toStatus: TaskStatus) {
+    this.tasks.update((taskList) =>
+      taskList.map((t) => {
+        if (t.id === task.id) {
+          return { ...t, status: toStatus };
+        }
+        return t;
+      })
+    );
   }
 
   // Delete task
   deleteTask(task: Task) {
-    this.tasks.update((taskList) => ({
-      todo: taskList.todo.filter((t) => t !== task),
-      doing: taskList.doing.filter((t) => t !== task),
-      completed: taskList.completed.filter((t) => t !== task),
-    }));
+    this.tasks.update((taskList) => taskList.filter((t) => t !== task));
   }
 }
